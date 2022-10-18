c20500731091af1cd921aa7d23419a41341217f7
Nur Fathona <81207714+nurfathona99@users.noreply.github.com>
2022-10-18 11:57:23 +0800

    Add files via upload

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+
+    public function index()
+    {
+        return view('layout/tampilan_login');
+    }
+
+
+    public function create()
+    { 
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
+}
